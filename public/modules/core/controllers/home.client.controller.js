64056472d8146c1a32fff8f20c1ae72a110a703d
Michael Olsen <jmolsen91@gmail.com>
2015-03-10 01:03:35 +0100

    asdf

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -1,11 +1,43 @@
 'use strict';
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-	function($scope, Authentication) {
-		// This provides Authentication context.
-		$scope.authentication = Authentication;
-		var youtube = document.createElement('script'); // use global document since Angular's $document is weak
-        youtube.src = 'https://apis.google.com/js/client.js?onload=OnLoadCallback';
-        document.body.appendChild(youtube);
+angular.module('core').controller('HomeController', ['$scope', 'Authentication', '$http', '$log',
+	function($scope, Authentication, $http, $log) {
+		$scope.init = function(soundcloud) {
+        	var SC = soundcloud;
+        	SC.get('/tracks', { q: 'buskers', license: 'cc-by-sa' }, function(tracks) {
+			 $log.info(tracks);
+			});
+    	};
+	    $log.info($scope.html_metadata);
+		$scope.searchYT = function () {
+	    	$http.get('https://www.googleapis.com/youtube/v3/search', {
+	        params: {
+	          key: 'AIzaSyBi6y6Vu-y_tDrpT5YdNsFAHKlgKD_TGuM',
+	          type: 'video',
+	          maxResults: '10',
+	          part: 'id,snippet',
+	          fields: 'items/id,items/snippet/title,items/snippet/description,items/snippet/thumbnails/default,items/snippet/channelTitle',
+	          q: $scope.YTquery
+	        }
+	      })
+	      .success( function (data) {
+	        $log.info(data);
+	        $scope.YTresults = data;
+	      })
+	      .error( function () {
+	        $log.info('Search error');
+	      });
+	    };
+	    $scope.addYTTrack = function (videoId) {
+	    	$log.info(videoId);
+		};
+
+		$scope.searchSC = function () {
+
+		};
+		$scope.addSCTrack = function () {
+
+		};
+	    $scope.authentication = Authentication;
 	}
 ]);
