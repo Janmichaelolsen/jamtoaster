1aa5ab6f6f0c9078484bb1ba2f18f98d27139c86
Michael Olsen <jmolsen91@gmail.com>
2015-03-11 18:43:30 +0100

    asdf

---
+++ public/modules/core/services/tracks.client.service.js
@@ -0,0 +1,15 @@
+'use strict';
+
+//Menu service used for managing  menus
+angular.module('core').service('TrackService', [
+
+    function() {
+        
+        // A private function for rendering decision 
+        var getTrack = function() {
+            var res = null;
+            
+            return res;
+        };
+    }
+]);
