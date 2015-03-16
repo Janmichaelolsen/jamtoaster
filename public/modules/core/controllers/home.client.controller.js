d2667c842c455cbe5360b56bd5c4c9ba7940f30f
Michael Olsen <jmolsen91@gmail.com>
2015-03-16 13:15:13 +0100

    asdf

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -2,15 +2,12 @@
 
 angular.module('core').controller('HomeController', ['$scope', 'Authentication', '$http', '$log', 'VideosService',
 	function($scope, Authentication, $http, $log, VideosService) {
+		//Youtube stuffs
 		var tag = document.createElement('script');
 		tag.src = "http://www.youtube.com/iframe_api";
 		var firstScriptTag = document.getElementsByTagName('script')[0];
 		firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-        SC.initialize({
-          client_id: '7ee4ea137d2c4782d07fc465eb841845'
-        });
-        
-	    function init() {
+		function init() {
 	      $scope.youtube = VideosService.getYoutube();
 	      $scope.playlist = true;
 	    }
@@ -20,9 +17,7 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	      	VideosService.launchPlayer(id, title);
 	      
 	    };
-
-		        
-		$scope.searchYT = function () {
+	    $scope.searchYT = function () {
 	    	$http.get('https://www.googleapis.com/youtube/v3/search', {
 	        params: {
 	          key: 'AIzaSyBi6y6Vu-y_tDrpT5YdNsFAHKlgKD_TGuM',
@@ -44,18 +39,43 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	    $scope.addYTTrack = function (videoId) {
 	    	$log.info(videoId);
 		};
-		
+
+
+	    //Soundcloud stuffs
+
+        SC.initialize({
+          client_id: '7ee4ea137d2c4782d07fc465eb841845'
+        });
+
+		var widgetIframe = document.getElementById('sc-widget'),
+		    widget       = SC.Widget(widgetIframe)
+
+		var newWidgetUrl = 'http://api.soundcloud.com/tracks/',
+		    CLIENT_ID    = '7ee4ea137d2c4782d07fc465eb841845'
+
 		$scope.searchSC = function () {
-			SC.get('/tracks', { q: $scope.SCquery, license: 'cc-by-sa' }, function(tracks) {
+			SC.get('/tracks', { q: $scope.SCquery }, function(tracks) {
 			$scope.$apply(function () {
-	            $scope.SCresults = tracks.slice(1, 11);
+	            $scope.SCresults = tracks;
 	        });
 			});
 		};
 		$scope.addSCTrack = function (soundId) {
+			widget.load('http://api.soundcloud.com/tracks/'+soundId, {
+			  show_artwork: false,
+			  auto_play:true,
+			  show_comments:false,
+			  buying:false,
+			  liking:false,
+			  sharing:false,
+			  show_playcount:false,
+			  show_user:false,
+			});
 			$log.info(soundId);
 		};
 		
+		//Hypem
+
 		$scope.searchHypem = function () {
 			$http.get('http://hypem.com/playlist/search/'+$scope.Hquery+'/json/1')
 			.success(function(tracks){
@@ -63,8 +83,20 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 			});
 		};
 		$scope.addHTrack = function (hypeId) {
+			widget.load('http://api.soundcloud.com/tracks/'+hypeId, {
+			  show_artwork: false,
+			  auto_play:true,
+			  show_comments:false,
+			  buying:false,
+			  liking:false,
+			  sharing:false,
+			  show_playcount:false,
+			  show_user:false,
+			});
 			$log.info(hypeId);
 		};
+
+		//Spotify
 	    
 	    $scope.searchSpotify = function () {
 	    	$http.get('https://api.spotify.com/v1/search?q='+$scope.Squery+'&type=track')
