c6560a798f6bc3787bd04fa8a139cf16d8ec4819
Jan Michael Olsen <jmolsen91@gmail.com>
2015-06-10 20:56:04 +0200

    asdf

--- public/modules/core/controllers/nav.client.controller.js
+++ public/modules/core/controllers/nav.client.controller.js
@@ -3,8 +3,6 @@
 angular.module('core').controller('NavController', ['$scope', 'Authentication', '$location', '$log',
 	function($scope, Authentication, $location, $log) {
 		$scope.authentication = Authentication;
-		$scope.searchClass = 'btn btn-success';
-		$scope.playClass = 'btn btn-default';
 
 		$scope.goToSearch = function() {
 			$location.path('/');
@@ -15,14 +13,14 @@ angular.module('core').controller('NavController', ['$scope', 'Authentication', 
 
 		$scope.$watch(function() { return $location.path(); }, function(){
 			if($location.path() === '/'){
-				$scope.searchClass = 'btn btn-success';
-				$scope.playClass = 'btn btn-default';
+				$scope.searchClass = 'toptabButton toptabButton-select';
+				$scope.playClass = 'toptabButton';
 			} else if($location.path() === '/play'){
-				$scope.playClass = 'btn btn-success';
-				$scope.searchClass = 'btn btn-default';
+				$scope.playClass = 'toptabButton toptabButton-select';
+				$scope.searchClass = 'toptabButton';
 			} else {
- 				$scope.playClass = 'btn btn-default';
-				$scope.searchClass = 'btn btn-default';
+ 				$scope.playClass = 'toptabButton';
+				$scope.searchClass = 'toptabButton';
 			}
 		});
 	}
